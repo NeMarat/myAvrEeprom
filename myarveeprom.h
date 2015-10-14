8447fdb5caaccfec7b440fad9715305d32293b62
NeMarat <maratn@gmail.com>
2015-10-14 22:50:44 +0300

    .

---
+++ myarveeprom.h
@@ -0,0 +1,22 @@
+#ifndef __MYARVEEPROM__
+#define __MYARVEEPROM__
+
+#include <avr/eeprom.h>
+
+enum {P_VALID = 17, P_INVALID};
+
+class MyEeprom {
+    public:
+      MyEeprom (uint32_t first_address, uint32_t max_address, uint16_t data_length, bool reload_pages = false);
+      void write_byte(uint8_t b, uint16_t relative_address);
+      uint8_t read_byte(uint16_t relative_address);
+
+    private:
+        uint16_t dataLength;
+        uint32_t firstAddress;
+        uint32_t maxAddress;
+        uint16_t currentOffset;
+};
+
+#endif // __MYARVEEPROM__
+
---
+++ myavreeprom.cpp
@@ -0,0 +1,54 @@
+#include "myarveeprom.h"
+
+MyEeprom::MyEeprom (uint32_t first_address, uint32_t max_address, uint16_t data_length, bool reload_pages = false) {
+  if (first_address + data_length + 1 > max_address) {
+      return null;
+  }
+  firstAddress = first_address;
+  maxAddress = max_address;
+  dataLength = data_length;
+
+  if (reload_pages == false) { currentOffset = 0; }
+  else {
+      uint16_t k = 0; bool found = false;
+      currentOffset = 0;
+      while (!eeprom_is_ready());
+      while ((first_address + k * (data_length + 1) < max_address) && (!found)) {
+          if (eeprom_read_byte((uint8_t*)(first_address + k * (data_length + 1))) == P_VALID) {
+              found = true;
+          }
+          currentOffset++;
+      }
+      if (!found) { currentOffset = 0; }
+  }
+};
+
+uint8_t MyEeprom::read_byte(uint16_t relative_address) {
+    while (!eeprom_is_ready());
+    return(eeprom_read_byte((uint8_t*)(firstAddress + currentOffset * (dataLength + 1) + 1 + relative_address)));
+}
+
+void MyEeprom::write_byte(uint8_t b, uint16_t relative_address) {
+    while (!eeprom_is_ready());
+    uint16_t pCurrentOffset = currentOffset;
+    if (firstAddress + (currentOffset + 1) * (dataLength + 1) > maxAddress) {
+        currentOffset = 0;
+    } else { currentOffset++; }
+    uint8_t tb = 0;
+    for (uint16_t i = 0; i < dataLength; i++) {
+        if (i == relative_address) {
+            cli();
+            eeprom_write_byte((uint8_t*)(firstAddress + currentOffset * (dataLength + 1) + 1 + i), b);
+            sei();
+        } else {
+          tb = eeprom_read_byte((uint8_t*)(firstAddress + pCurrentOffset * (dataLength + 1) + 1 + i));
+          cli();
+          eeprom_write_byte((uint8_t*)(firstAddress + currentOffset * (dataLength + 1) + 1 + i), tb);
+          sei();
+        }
+    }
+    cli();
+    eeprom_write_byte((uint8_t*)(firstAddress + currentOffset * (dataLength + 1)), P_VALID);
+    eeprom_write_byte((uint8_t*)(firstAddress + pCurrentOffset * (dataLength + 1)), P_INVALID);
+    sei();
+}
